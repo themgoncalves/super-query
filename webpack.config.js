b7b8259d49107802f8885cbc2f739b5a1744f4cf
Marcos Gonçalves <marcos.goncalves@wimdu.com>
2018-06-12 13:45:43 +0200

    :book: Added JSDoc documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-require('dotenv').config(); // Loads environment variables from a .env file into process.env
 const webpack = require('webpack');
-const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,7 +6,7 @@ module.exports = {
     index:
       [
         'babel-polyfill',
-        './src/index.js',
+        './source/index.js',
       ],
   },
   output: {
@@ -26,24 +24,11 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components|public\/)/,
         loader: 'babel-loader',
-      }
-    ]
-  },
-  performance: {
-    hints: 'error',
-    maxAssetSize: 250000,
-    maxEntrypointSize: 400000,
-    assetFilter: function (assetFilename) {
-      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
-    },
-  },
-  optimization: {
-    nodeEnv: 'production',
-    minimize: true,
+      },
+    ],
   },
   target: 'web',
   plugins: [
-    new WebpackCleanupPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
